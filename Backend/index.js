78097d7ff1af0ffae0c011803af19c44340ffb87
Dhanish Kumar <dhanishkumar2001@gmail.com>
2025-02-22 16:36:17 +0530

    First comits

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+const connectToMongo=require('./db');
+const express = require('express')
+
+connectToMongo();
+const app = express()
+const port = 3000
+
+app.use('/api/auth', require('./Routes/auth'));
+app.use('/api/notes', require('./Routes/notes'));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
