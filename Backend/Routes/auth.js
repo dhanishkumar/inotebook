f88ef3d16cb3865eba15840bad87ccaafd7586cc
Dhanish Kumar <dhanishkumar2001@gmail.com>
2025-02-27 13:15:15 +0530

    Third commit

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -1,13 +1,29 @@
-const express=require('express');
-const router=express.Router();
-const User =require('../models/User');
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
-//Create a User using:Post "/api/auth"
+router.post('/', [
+    body('name').isLength({ min: 5 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user=User(req.body);
-    user.save()
-    res.send(req.body);
-})
-module.exports=router
+
+    //create a new user
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user))
+        .catch(err => {
+            console.log(err)
+            res.json({ error: 'Please enter a unique for email', message: err.message })
+        });
+});
+
+module.exports = router;
